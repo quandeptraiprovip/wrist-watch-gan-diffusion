40886f4b407df0b1c79aa1e1e582cf841c1a9da3
quandeptraiprovip <ttmq38@gmail.com>
2025-11-26 22:27:11 +0700

    add stylegan file

--- crawl/main.py
+++ crawl/main.py
@@ -72,24 +72,24 @@ def download_and_resize(url, save_path):
 page = 346
 os.makedirs("images", exist_ok=True)
 
-# while True:
-url = "https://vuahanghieu.com/dong-ho" if page == 1 else f"https://vuahanghieu.com/dong-ho?page={page}"
-print("\n🔍 CRAWL:", url)
+while True:
+    url = "https://vuahanghieu.com/dong-ho" if page == 1 else f"https://vuahanghieu.com/dong-ho?page={page}"
+    print("\n🔍 CRAWL:", url)
 
-html = fetch_page(url)
-if not html:
-    print("⛔ HTML rỗng → STOP.")
-    # break
+    html = fetch_page(url)
+    if not html:
+        print("⛔ HTML rỗng → STOP.")
+        # break
 
-imgs = extract_images(html)
-if len(imgs) == 0:
-    print("⛔ Hết sản phẩm → STOP.")
-    # break
+    imgs = extract_images(html)
+    if len(imgs) == 0:
+        print("⛔ Hết sản phẩm → STOP.")
+        # break
 
-print("📸 Tìm được:", len(imgs), "ảnh")
+    print("📸 Tìm được:", len(imgs), "ảnh")
 
-for idx, link in enumerate(imgs):
-    save_path = f"images/p{page}_{idx}.jpg"
-    download_and_resize(link, save_path)
+    for idx, link in enumerate(imgs):
+        save_path = f"images/p{page}_{idx}.jpg"
+        download_and_resize(link, save_path)
 
-page += 1
+    page += 1
